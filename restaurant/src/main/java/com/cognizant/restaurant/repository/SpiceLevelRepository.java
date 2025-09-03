e52f8b528e736adbd7a763c6917f8b78f7678329
Patel <978337@cognizant.com>
2025-09-02 20:00:10 -0400

    Get all items has a better return schema now, Options is all sorted and flattened as well, Featured products shcema works as well.

---
+++ restaurant/src/main/java/com/cognizant/restaurant/repository/SpiceLevelRepository.java
@@ -0,0 +1,9 @@
+package com.cognizant.restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cognizant.restaurant.entity.SpiceLevel;
+
+public interface SpiceLevelRepository  extends JpaRepository<SpiceLevel, Long>{
+
+}
